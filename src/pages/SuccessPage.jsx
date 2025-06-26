2a60723d6c3544f95adb816d5537f6deed1e53d7
OwnCompanyself <owncompany27@gmail.com>
2025-06-26 18:32:07 +0530

    Update SuccessPage.jsx

--- src/pages/SuccessPage.jsx
+++ src/pages/SuccessPage.jsx
@@ -48,8 +48,6 @@ const SuccessPage = () => {
           className="w-full bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded-full"
           onClick={() => window.location.reload()}
         >
-          Back to Home
-        </button>
       </div>
     </div>
   );
